d3f8c0435e8795702cf2fb3dd5c7060f943db8ad
itsarunkumarx <arunkumarpalani428@gmail.com>
2026-08-01 20:48:47 +0530

    Configure CSRF_TRUSTED_ORIGINS and ALLOWED_HOSTS for Render deployment

--- Taxigo/settings.py
+++ Taxigo/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-704m38^mizd!j4wplnl+v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+    "https://*.vercel.app",
+    "http://127.0.0.1",
+    "http://localhost",
+]
 
 
 # Application definition
